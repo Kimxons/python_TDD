9a195d57ded9bab38b688c881d036f02013a8eba
kimxons <kitongameshack9@gmail.com>
2020-10-01 16:10:44 +0300

    modified text

--- README.txt
+++ README.txt
@@ -8,3 +8,9 @@ This is implemented using Stack Data structure in python.
 
 Use Case:
 Allows the user to input a word or word phrase.By traversing the word or word phrase either backwards or forward, the word should be read the same from front-to-back and from back-to-front.
+
+
+A palindrome is a word that is read the same from front-to-back and from back-to-front.
+implemented by traversing the given word or phrase forwards and backwards
+to check whether it is the same
+using a stack D.S to find palindromes
--- palindrome_test.py
+++ palindrome_test.py
@@ -4,25 +4,21 @@ from palindrome import list
 
 class TestPalindrome(unittest2.TestCase):
 
-    #tests true if and only if the value is the same in reverse or the normal forward mode
     def test_is_palindrome_true(self):
         value = list.is_palindrome('madam')
         self.assertEquals(value, True)
 
-    #tests false if the word and it's reversed version are not the same
     def test_is_palindrome_false(self):
         value = list.is_palindrome('man')
         self.assertEquals(value, False)
 
-    #the reversed word
     def test_reverse_normal(self):
         value = list.reverse('man')
         self.assertEquals(value, 'nam')
 
-   # Raise type errors whenever necessary
     def test_reverse_error(self):
         list_of_bad_value = [
-        345,
+        100,
         None,
         ]
         for bad_value in list_of_bad_value:
