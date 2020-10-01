dbadcbe5a0b0272ba643a81477b74f8963586c53
kimxons <kitongameshack9@gmail.com>
2020-10-01 16:16:24 +0300

    added str_pal.py

---
+++ str_pal.py
@@ -0,0 +1,7 @@
+def palCheck(name):
+    if(name == name[::-1]):
+        print("Its a palidrome")
+    else:
+        print("Not a palindrome")
+name=input("Enter a word: ")
+palCheck(name)           
